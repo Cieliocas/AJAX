f8cbb9ce74be4e0ecfba8e53b3b21aa87574bf05
Cieliocas <francielioevangelista@hotmail.com>
2024-12-03 03:44:50 -0300

    test error

--- main.js
+++ main.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
 
     $('#btn-buscar-cep').click(function() {
         const cep = $('#cep').val();
-        const endpoint = `https://viacep.com.br/ws/${cep}/json/`;
+        const endpoint = `https://viacep.com.br/ws/${cep}/json/resposta`;
         const botao = $(this);
         $(botao).find('i').addClass('d-none');
         $(botao).find('span').removeClass('d-none');
@@ -38,17 +38,22 @@ $(document).ready(function() {
         fetch(endpoint).then(function(resposta) {
             return resposta.json()
         })
-            .then(function(json) {
-                const logradouro = json.logradouro;
-                const bairro = json.bairro;
-                const cidade = json.localidade;
-                const estado = json.uf;
-                const endereco = `${logradouro}, ${bairro}, ${cidade} - ${estado}`;
-                $('#endereco').val(endereco);
+        .then(function(json) {
+            const logradouro = json.logradouro;
+            const bairro = json.bairro;
+            const cidade = json.localidade;
+            const estado = json.uf;
+            const endereco = `${logradouro}, ${bairro}, ${cidade} - ${estado}`;
+            $('#endereco').val(endereco);
         })
+        .catch(function(erro) {
+            alert('Erro ao buscar o CEP');
+        })
+        .finally(function() {
             setTimeout(function() {
                 $(botao).find('i').removeClass('d-none');
                 $(botao).find('span').addClass('d-none');
-            }, 2000);
+            }, 1000);
+        });
     })
 })
