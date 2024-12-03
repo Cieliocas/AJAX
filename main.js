9388dbef48055edc9b423502f32e930eed96a646
Cieliocas <francielioevangelista@hotmail.com>
2024-12-03 03:13:40 -0300

    added mask jquery to cep field

--- main.js
+++ main.js
@@ -11,9 +11,15 @@
 // });
 
 $(document).ready(function() {
+    $('#cep').mask('00000-000');
+
     $('#btn-buscar-cep').click(function() {
         const cep = $('#cep').val();
         const endpoint = `https://viacep.com.br/ws/${cep}/json/`;
+        const botao = $(this);
+        $(botao).find('i').addClass('d-none');
+        $(botao).find('span').removeClass('d-none');
+
 
         $.ajax(endpoint).done(function(resposta) {
             const logradouro = resposta.logradouro;
@@ -22,6 +28,11 @@ $(document).ready(function() {
             const estado = resposta.uf;
             const endereco = `${logradouro}, ${bairro}, ${cidade} - ${estado}`;
             $('#endereco').val(endereco);
+
+            setTimeout(function() {
+                $(botao).find('i').removeClass('d-none');
+                $(botao).find('span').addClass('d-none');
+            }, 2000);
         });
     });
 });
